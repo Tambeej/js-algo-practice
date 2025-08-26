9e1f4349e4f626b3810879ddd80e5ddc7e28fee0
Tambeej <bejella@gmail.com>
2025-08-26 14:07:58 +0300

    added another case

--- Very_Hard/ascending.js
+++ Very_Hard/ascending.js
@@ -18,27 +18,43 @@ function splitStringIntoEqualParts(str, partLength) {
   for (let i = 0; i < str.length; i += partLength) {
     result.push(str.slice(i, i + partLength));
   }
+
+  if (result.length > 1 && result[result.length - 1].length < partLength) {
+    result[result.length - 2] += result.pop();
+  }
+
   return result;
+}
+
+function areAllCharsNine(str) {
+  return /^9*$/.test(str);
 }
 
 function ascending(str) {
   let flag = true;
+  let cleanStr = "";
   for (let i = 1; i <= str.length / 2; i++) {
-    if (!(str.length % i == 0)) {
-      continue;
-    }
     const parts = splitStringIntoEqualParts(str, i);
     for (let j = 0; j < parts.length - 1; j++) {
+      if (areAllCharsNine(parts[j])) {
+        cleanStr = parts.slice(j + 1, parts.length).join("");
+        parts.splice(j + 1, parts.length - j + 1);
+        parts.push(...splitStringIntoEqualParts(cleanStr, i + 1));
+      }
       if (Number(parts[j]) + 1 != Number(parts[j + 1])) {
         flag = false;
         break;
       }
     }
     if (flag) {
+      console.log(`true: ${str}`);
       return true;
     }
     flag = true;
   }
+  console.log(`false: ${str}`);
   return false;
 }
+
+console.log(ascending("919920921"));
 exports.solution = ascending;
