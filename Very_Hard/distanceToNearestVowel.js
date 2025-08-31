7f1db20e8646d854c8ba2afbad16c6cc1c2f3f21
Tambeej <bejella@gmail.com>
2025-08-31 23:30:14 +0300

    resolved distanceToNearestVowel

--- Very_Hard/distanceToNearestVowel.js
+++ Very_Hard/distanceToNearestVowel.js
@@ -15,8 +15,61 @@ Strings will be lowercased.
 Vowels are: a, e, i, o, u.
 */
 
-function distanceToNearestVowel(/*args*/) {
-  //your code
+
+
+function distanceToNearestVowel(str) {
+  let result = [];
+  let leftToRight = [];
+  let rightToLeft = [];
+  let vowels = ["a", "e", "i", "o", "u"];
+  let flag = false;
+  let counter = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (!flag) {
+      if (!vowels.includes(str.charAt(i))) {
+        leftToRight.push(str.length - 1);
+      } else {
+        leftToRight.push(0);
+        flag = true;
+      }
+    } else {
+      if (!vowels.includes(str.charAt(i))) {
+        leftToRight.push(++counter);
+      } else {
+        leftToRight.push(0);
+        counter = 0;
+      }
+    }
+  }
+  flag = false;
+  counter = 0;
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (!flag) {
+      if (!vowels.includes(str.charAt(i))) {
+        rightToLeft.push(str.length - 1);
+      } else {
+        rightToLeft.push(0);
+        flag = true;
+      }
+    } else {
+      if (!vowels.includes(str.charAt(i))) {
+        rightToLeft.push(++counter);
+      } else {
+        rightToLeft.push(0);
+        counter = 0;
+      }
+    }
+  }
+
+  rightToLeft.reverse();
+  for (let i = 0; i < str.length; i++) {
+    result.push(Math.min(leftToRight[i], rightToLeft[i]));
+  }
+
+  return result;
 }
+
+console.log(distanceToNearestVowel("bba"));
 
 exports.solution = distanceToNearestVowel;
