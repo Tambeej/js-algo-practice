8f99ae71c09193416ff402f2387d51865a6ccaa7
Tambeej <bejella@gmail.com>
2025-09-02 13:52:08 +0300

    solved join

--- Very_Hard/join.js
+++ Very_Hard/join.js
@@ -18,8 +18,50 @@ Notes
 More specifically, look at the overlap between the previous words ending letters and the next word's beginning letters.
 */
 
-function join( /*args*/ ) {
-  //your code
+function findLongestString(arr) {
+  if (arr.length === 0) {
+    return null; // Or throw an error, depending on desired behavior for empty arrays
+  }
+
+  let longestString = arr[0]; // Initialize with the first string
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i].length > longestString.length) {
+      longestString = arr[i]; // Update if a longer string is found
+    }
+  }
+
+  return longestString.length;
 }
+
+function overlap(word1, word2) {
+  let sub = "";
+  for (let i = word1.length - 1; i >= 0; i--) {
+    if (word2.includes(word1.substring(i))) {
+      sub = word1.substring(i);
+    } else {
+      if (word1.endsWith(sub)) {
+        return [word1 + word2.substring(sub.length, word2.length), sub.length];
+      }
+      return [word1 + word2, 0];
+    }
+  }
+  return [word1 + word2, 0];
+}
+
+function join(arr) {
+  let min = findLongestString(arr);
+  let currentWord = arr[0];
+  let currentMin;
+  for (let i = 1; i < arr.length; i++) {
+    [currentWord, currentMin] = overlap(currentWord, arr[i]);
+    if (currentMin < min) {
+      min = currentMin;
+    }
+  }
+  return [currentWord, min];
+}
+
+console.log(join(["happy", "python", "honey", "yelp", "plank", "lanky"]));
 
 exports.solution = join;
